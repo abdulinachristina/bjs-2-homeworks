5034745dff8b8ddcd09b24a370e793ebab877fb8
Christina <abdulinachris@icloud.com>
2024-12-14 20:40:43 +0500

    1 задача 1 задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,14 @@
-"use strict"
+'use strict';
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+let arr = [];
+let d = b**2 - 4 * a * c;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+if (d < 0) {
+arr = [];
+} else if (d === 0) {
+arr = [-b / (2 * a)];
+} else if (d > 0) {
+arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+}
+return arr;
 }
